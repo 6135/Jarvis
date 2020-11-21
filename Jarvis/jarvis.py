d5d5ea9f39542ae9b2f5f877d31f93bd041976e3
6135 <guibscosta@hotmail.com>
2020-11-21 22:49:53 +0000

    Update jarvis.py

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -18,49 +18,50 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Grab the API token from the .env file.
 async def sysinfo(client, message):
-	if message.author.id != 522010502138822666:
+	if message.author.id == 522010502138822666 or message.author.id == 362697372758966312:
+
+		def get_size(bytes, suffix="B"):
+			factor = 1024
+			for unit in ["", "K", "M", "G", "T", "P"]:
+				if bytes < factor:
+					return f"{bytes:.2f}{unit}{suffix}"
+				bytes /= factor
+		uname = platform.uname()
+		botRsp = "```"
+		botRsp += f"="*5 + f"System Information" + f"="*5 +\
+				f"\nSystem: {uname.system}\n" +\
+				f"Node Name: N/A\n" +\
+				f"Release: {uname.release}\n" +\
+				f"Version: {uname.version}\n" +\
+				f"Machine: {uname.machine}\n" +\
+				f"Processor: {uname.processor}\n"
+		boot_time_timestamp = psutil.boot_time()
+		bt = datetime.fromtimestamp(boot_time_timestamp)
+		cpufreq = psutil.cpu_freq()
+		botRsp += f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}\n"
+		botRsp += f"="*5 + f"CPU Information" + f"="*5 +"\n" +\
+			f"Physical cores: {psutil.cpu_count(logical=False)}\n"+\
+			f"Total cores: {psutil.cpu_count(logical=True)}\n"+\
+			f"Max Frequency: {cpufreq.max:.2f}Mhz\n"+\
+			f"Min Frequency: {cpufreq.min:.2f}Mhz\n"+\
+			f"Current Frequency: {cpufreq.current:.2f}Mhz\n"
+
+		svmem = psutil.virtual_memory()
+		swap = psutil.swap_memory()
+		botRsp += f"="*5 + f"Memory Information" + f"="*5 +"\n" +\
+			f"Total: {get_size(svmem.total)}\n"+\
+			f"Available: {get_size(svmem.available)}\n"+\
+			f"Used: {get_size(svmem.used)}\n"+\
+			f"Percentage: {svmem.percent}%\n"+\
+			f"="*20 + f"SWAP" + f"="*20 +"\n" +\
+			f"Total: {get_size(swap.total)}\n" +\
+			f"Free: {get_size(swap.free)}\n"+\
+			f"Used: {get_size(swap.used)}\n"+\
+			f"Percentage: {swap.percent}%\n"
+
+		await message.channel.send(botRsp+"```")
+	else:
 		await message.channel.send("You don't have permission to use this command")
-		return
-	def get_size(bytes, suffix="B"):
-		factor = 1024
-		for unit in ["", "K", "M", "G", "T", "P"]:
-			if bytes < factor:
-				return f"{bytes:.2f}{unit}{suffix}"
-			bytes /= factor
-	uname = platform.uname()
-	botRsp = "```"
-	botRsp += f"="*5 + f"System Information" + f"="*5 +\
-			f"\nSystem: {uname.system}\n" +\
-			f"Node Name: N/A\n" +\
-			f"Release: {uname.release}\n" +\
-			f"Version: {uname.version}\n" +\
-			f"Machine: {uname.machine}\n" +\
-			f"Processor: {uname.processor}\n"
-	boot_time_timestamp = psutil.boot_time()
-	bt = datetime.fromtimestamp(boot_time_timestamp)
-	cpufreq = psutil.cpu_freq()
-	botRsp += f"Boot Time: {bt.year}/{bt.month}/{bt.day} {bt.hour}:{bt.minute}:{bt.second}\n"
-	botRsp += f"="*5 + f"CPU Information" + f"="*5 +"\n" +\
-		f"Physical cores: {psutil.cpu_count(logical=False)}\n"+\
-		f"Total cores: {psutil.cpu_count(logical=True)}\n"+\
-		f"Max Frequency: {cpufreq.max:.2f}Mhz\n"+\
-		f"Min Frequency: {cpufreq.min:.2f}Mhz\n"+\
-		f"Current Frequency: {cpufreq.current:.2f}Mhz\n"
-
-	svmem = psutil.virtual_memory()
-	swap = psutil.swap_memory()
-	botRsp += f"="*5 + f"Memory Information" + f"="*5 +"\n" +\
-		f"Total: {get_size(svmem.total)}\n"+\
-		f"Available: {get_size(svmem.available)}\n"+\
-		f"Used: {get_size(svmem.used)}\n"+\
-		f"Percentage: {svmem.percent}%\n"+\
-		f"="*20 + f"SWAP" + f"="*20 +"\n" +\
-		f"Total: {get_size(swap.total)}\n" +\
-		f"Free: {get_size(swap.free)}\n"+\
-		f"Used: {get_size(swap.used)}\n"+\
-		f"Percentage: {swap.percent}%\n"
-
-	await message.channel.send(botRsp+"```")
 
 async def reactBack(client,message):
 	embed=discord.Embed(title="React to this message", color=0x80ff00)
